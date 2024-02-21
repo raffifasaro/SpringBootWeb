9d40e370978e8ecf75e9f31e5a95b31850ce9ada
RaphaelKlein <raphael.klein@tum.de>
2024-02-21 21:39:13 +0100

    first

---
+++ SpringWeb/src/test/java/amogus/example/springweb/SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package amogus.example.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
